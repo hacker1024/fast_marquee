a219bbd8a91692f7078e2c32bbf75de83c9bd34c
hacker1024 <hacker1024@users.sourceforge.net>
2021-04-10 20:22:45 +1000

    Update example lints

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,10 +2,12 @@ import 'package:fast_marquee/fast_marquee.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -15,12 +17,14 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     Widget buildSimpleMarquee() {
